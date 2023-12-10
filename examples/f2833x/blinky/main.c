e3128bbed29366909afeffab8b8d9b2baa88513c
sicris <sicris.embay@gmail.com>
2023-12-10 20:08:22 +0800

    + disabled Watchdog timer on code starting point + initialized PIE Vector table with default ISR function

--- examples/f2833x/blinky/main.c
+++ examples/f2833x/blinky/main.c
@@ -130,25 +130,87 @@ static void blinkyTask(void * pvParam)
 }
 
 
+interrupt void DefualtISR(void)
+{
+    asm ("      ESTOP0");
+    for(;;);
+}
+
+
+void InitPieCtrl(void)
+{
+    //
+    // Disable Interrupts at the CPU level
+    //
+    DINT;
+
+    //
+    // Disable the PIE
+    //
+    PieCtrlRegs.PIECTRL.bit.ENPIE = 0;
+
+    //
+    // Clear all PIEIER registers
+    //
+    PieCtrlRegs.PIEIER1.all = 0;
+    PieCtrlRegs.PIEIER2.all = 0;
+    PieCtrlRegs.PIEIER3.all = 0;
+    PieCtrlRegs.PIEIER4.all = 0;
+    PieCtrlRegs.PIEIER5.all = 0;
+    PieCtrlRegs.PIEIER6.all = 0;
+    PieCtrlRegs.PIEIER7.all = 0;
+    PieCtrlRegs.PIEIER8.all = 0;
+    PieCtrlRegs.PIEIER9.all = 0;
+    PieCtrlRegs.PIEIER10.all = 0;
+    PieCtrlRegs.PIEIER11.all = 0;
+    PieCtrlRegs.PIEIER12.all = 0;
+
+    //
+    // Clear all PIEIFR registers
+    //
+    PieCtrlRegs.PIEIFR1.all = 0;
+    PieCtrlRegs.PIEIFR2.all = 0;
+    PieCtrlRegs.PIEIFR3.all = 0;
+    PieCtrlRegs.PIEIFR4.all = 0;
+    PieCtrlRegs.PIEIFR5.all = 0;
+    PieCtrlRegs.PIEIFR6.all = 0;
+    PieCtrlRegs.PIEIFR7.all = 0;
+    PieCtrlRegs.PIEIFR8.all = 0;
+    PieCtrlRegs.PIEIFR9.all = 0;
+    PieCtrlRegs.PIEIFR10.all = 0;
+    PieCtrlRegs.PIEIFR11.all = 0;
+    PieCtrlRegs.PIEIFR12.all = 0;
+
+    // Disable CPU interrupts and clear all CPU interrupt flags:
+    IER = 0x0000;
+    IFR = 0x0000;
+
+    volatile Uint32 *dest = (void *)&PieVectTable;
+    EALLOW;
+    for (uint16_t i = 0; i < 128; i++) {
+        *dest++ = (Uint32)&DefualtISR;
+    }
+    EDIS;
+
+    /* Enable the PIE Vector Table */
+    PieCtrlRegs.PIECTRL.bit.ENPIE = 1;
+}
+
+
 static TaskHandle_t blinkyTaskHandle = NULL;
 
 void main()
 {
+    InitPieCtrl();
+
     /*
      * Copy ramfuncs section
      */
     memcpy(&RamfuncsRunStart, &RamfuncsLoadStart,
             &RamfuncsLoadEnd - &RamfuncsLoadStart);
 
-    /* Configure Core Frequency */
-    portDISABLE_INTERRUPTS();
-    IER = 0x0000;
-    IFR = 0x0000;
-
-    /* Enable the PIE Vector Table */
-    PieCtrlRegs.PIECTRL.bit.ENPIE = 1;
-
     InitFlashWaitState();
+    /* Configure Core Frequency */
     configure_core_pll(0xA);
 
 
