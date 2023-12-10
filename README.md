b093af18bb4f5a6272b8b081e2404c742a913c15
sicris <sicris.embay@gmail.com>
2023-12-10 13:43:01 +0800

    + implemented blinky as simple FreeRTOS test

---
+++ README.md
@@ -0,0 +1,13 @@
+# Test of FreeRTOS port on TMS320F28335
+This contains examples CCS (Code Composer Studio) projects that are used to test FreeRTOS port on TMS320F28335 DSP.
+
+# Example Projects
+## Blinky
+This is a simple example that blinks the LED (500ms ON and 1500ms OFF).
+### Hardware Test Setup
+The setup is composed of C2000 DIMM100 Experimenter kit with TMDSCNCD28335 control card.
+
+<p align="center">
+  <img src="https://github.com/sicrisembay/c2000ware-FreeRTOS-F28335-Test/blob/main/doc/img/TMDSDOCK28335.png">
+</p>
+
--- examples/f2833x/blinky/main.c
+++ examples/f2833x/blinky/main.c
@@ -5,7 +5,156 @@
  *      Author: Sicris
  */
 
+#include "FreeRTOS.h"
+#include "task.h"
+
+extern unsigned int RamfuncsLoadStart;
+extern unsigned int RamfuncsLoadEnd;
+extern unsigned int RamfuncsRunStart;
+
+
+static void configure_core_pll(uint16_t val)
+{
+    /* Make sure the PLL is not running in limp mode */
+    if (SysCtrlRegs.PLLSTS.bit.MCLKSTS != 0)
+    {
+       /*
+        * Missing external clock has been detected
+        * Replace this line with a call to an appropriate
+        * SystemShutdown(); function.
+        */
+       asm("        ESTOP0");
+    }
+
+    /* Change the PLLCR */
+    if (SysCtrlRegs.PLLCR.bit.DIV != val)
+    {
+
+       EALLOW;
+       /* Before setting PLLCR turn off missing clock detect logic */
+       SysCtrlRegs.PLLSTS.bit.MCLKOFF = 1;
+       SysCtrlRegs.PLLCR.bit.DIV = val;
+       EDIS;
+
+       /*
+        * Wait for PLL to lock.
+        * During this time the CPU will switch to OSCCLK/2 until
+        * the PLL is stable.  Once the PLL is stable the CPU will
+        *
+        * Wait for the PLL lock bit to be set.
+        */
+       while(SysCtrlRegs.PLLSTS.bit.PLLLOCKS == 0) {
+           /*
+            * Note: The watchdog should be fed within
+            * the loop via ServiceDog().
+            */
+       }
+
+       EALLOW;
+       SysCtrlRegs.PLLSTS.bit.MCLKOFF = 0;
+       EDIS;
+     }
+}
+
+
+#pragma CODE_SECTION(InitFlashWaitState, "ramfuncs");
+static void InitFlashWaitState(void)
+{
+    EALLOW;
+
+    //
+    // Enable Flash Pipeline mode to improve performance
+    // of code executed from Flash.
+    //
+    FlashRegs.FOPT.bit.ENPIPE = 1;
+
+    //
+    //                CAUTION
+    // Minimum waitstates required for the flash operating
+    // at a given CPU rate must be characterized by TI.
+    // Refer to the datasheet for the latest information.
+    //
+
+    //
+    // Set the Paged Waitstate for the Flash
+    //
+    FlashRegs.FBANKWAIT.bit.PAGEWAIT = 5;
+
+    //
+    // Set the Random Waitstate for the Flash
+    //
+    FlashRegs.FBANKWAIT.bit.RANDWAIT = 5;
+
+    //
+    // Set the Waitstate for the OTP
+    //
+    FlashRegs.FOTPWAIT.bit.OTPWAIT = 8;
+    //
+    //                CAUTION
+    // ONLY THE DEFAULT VALUE FOR THESE 2 REGISTERS SHOULD BE USED
+    //
+    FlashRegs.FSTDBYWAIT.bit.STDBYWAIT = 0x01FF;
+    FlashRegs.FACTIVEWAIT.bit.ACTIVEWAIT = 0x01FF;
+
+    EDIS;
+
+    //
+    // Force a pipeline flush to ensure that the write to
+    // the last register configured occurs before returning.
+    //
+    asm(" RPT #7 || NOP");
+}
+
+
+static void blinkyTask(void * pvParam)
+{
+    TickType_t xLastWakeTime;
+
+    EALLOW;
+    SysCtrlRegs.PCLKCR3.bit.GPIOINENCLK = 1;
+    GpioCtrlRegs.GPBPUD.bit.GPIO34 = 1;
+    GpioCtrlRegs.GPBDIR.bit.GPIO34 = 1;
+    GpioCtrlRegs.GPBMUX1.bit.GPIO34 = 0;
+    EDIS;
+
+    xLastWakeTime = xTaskGetTickCount();
+
+    while(1) {
+        /* ON LED */
+        GpioDataRegs.GPBCLEAR.bit.GPIO34 = 1;
+        vTaskDelayUntil(&xLastWakeTime, 500);
+        /* OFF LED */
+        GpioDataRegs.GPBSET.bit.GPIO34 = 1;
+        vTaskDelayUntil(&xLastWakeTime, 1500);
+    }
+}
+
+
+static TaskHandle_t blinkyTaskHandle = NULL;
+
 void main()
 {
+    /*
+     * Copy ramfuncs section
+     */
+    memcpy(&RamfuncsRunStart, &RamfuncsLoadStart,
+            &RamfuncsLoadEnd - &RamfuncsLoadStart);
+
+    /* Configure Core Frequency */
+    portDISABLE_INTERRUPTS();
+    IER = 0x0000;
+    IFR = 0x0000;
+
+    /* Enable the PIE Vector Table */
+    PieCtrlRegs.PIECTRL.bit.ENPIE = 1;
+
+    InitFlashWaitState();
+    configure_core_pll(0xA);
+
+
+    xTaskCreate(blinkyTask, "blinky", 512, NULL, 1, &blinkyTaskHandle);
+    vTaskStartScheduler();
+
+    /* Should not reach here */
     while(1);
 }
